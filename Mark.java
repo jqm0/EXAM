d526a4512f7e66e3c09c41aaabb9469ceedcf5cc
jqm0 <121156533+jqm0@users.noreply.github.com>
2022-12-25 03:32:37 -0800

    Update Mark.java
    add contractor with default values for instance variables -

--- Mark.java
+++ Mark.java
@@ -3,7 +3,11 @@ package firstTask;
 public class Mark {
 	private int markMath ;
 	private int physics ;
-	
+public Mark() {
+		
+		this.markMath = 60;
+		this.physics = 70;
+	}
 	public int getMathMark() {
 		return markMath ;
 	}
